e5554d7d4713a8f1a7d87a27754ec01b2fb588a9
jurlep <ratedrko@yandex.ru>
2021-05-29 12:18:13 +0300

    Binary Search

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -9,5 +9,29 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
-    print(elem, arr)
-    return None
+    first = 0
+    last = len(arr) - 1
+    while first <= last:
+        mid = (last + first) // 2
+        if elem == arr[mid]:
+            break
+        elif elem < arr[mid]:
+            last = mid - 1
+        else:
+            first = mid + 1
+    else:
+        return None
+
+    while mid > 0 and arr[mid - 1] == elem:
+        mid -= 1
+    return mid
+
+
+def main():
+    elem = 10
+    arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    print(binary_search(elem, arr))
+
+
+if __name__ == '__main__':
+    main()
