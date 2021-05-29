b29b1cfaab9587c891e1584d8eb7f9b8f754bf0d
jurlep <ratedrko@yandex.ru>
2021-05-29 12:48:28 +0300

    Fib number

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -5,8 +5,14 @@ def fib_recursive(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
+    if n < 0:
+        raise ValueError
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib_recursive(n - 1) + fib_recursive(n - 2)
 
 
 def fib_iterative(n: int) -> int:
@@ -16,5 +22,15 @@ def fib_iterative(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
+    if n < 0:
+        raise ValueError
+    a, b = 0, 1
+    while n > 0:
+        a, b = b, a + b
+        n -= 1
+    return a
+
+
+if __name__ == '__main__':
+    print(fib_recursive(10))
+    print(fib_iterative(10))
