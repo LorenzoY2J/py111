3ac1beca90b05896e8de61e9ed8a54bc5d87aaa9
jurlep <ratedrko@yandex.ru>
2021-05-29 12:17:26 +0300

    Binary Search

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -9,28 +9,29 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
-    low = arr[0]
-    high = len(arr) - 1
-    mid = len(arr) // 2
-    mid_value = arr[mid]
-    while mid_value != elem:
-        if elem > mid_value:
-            mid = mid + 1
-            mid_value = (mid_value + high) // 2
-        elif elem < mid_value:
-            mid = mid - 1
-            mid_value = (mid_value + low) // 2
+    first = 0
+    last = len(arr) - 1
+    while first <= last:
+        mid = (last + first) // 2
+        if elem == arr[mid]:
+            break
+        elif elem < arr[mid]:
+            last = mid - 1
         else:
-            arr[mid] = mid_value
-    return arr[elem]
+            first = mid + 1
+    else:
+        return None
+
+    while mid > 0 and arr[mid - 1] == elem:
+        mid -= 1
+    return mid
 
 
 def main():
-    elem = 2
-    arr = [1, 2, 3, -11, 1, 4, -7, 8]
+    elem = 10
+    arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     print(binary_search(elem, arr))
 
 
 if __name__ == '__main__':
     main()
-
