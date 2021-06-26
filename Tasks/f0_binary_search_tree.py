a0925fbbc83cdf38c91378362d62865dfc4d9172
jurlep <ratedrko@yandex.ru>
2021-06-26 13:24:02 +0300

    Бинарный поиск по дереву

--- Tasks/f0_binary_search_tree.py
+++ Tasks/f0_binary_search_tree.py
@@ -3,8 +3,36 @@ You can do it either with networkx ('cause tree is a graph)
 or with dicts (smth like {'key': 0, value: 123, 'left': {...}, 'right':{...}})
 """
 
-from typing import Any, Optional, Tuple
+from typing import Any, Optional, Tuple, Dict
+
 # import networkx as nx
+
+root: Optional[Dict] = None
+
+
+def _create_node(key, value, left=None, right=None):
+    return {
+        "key": key,
+        "value": value,
+        "left": left,
+        "right": right
+    }
+
+
+def _find(key: int) -> Tuple[Optional[dict], Optional[dict]]:
+    prev_root = None
+    current_root = root
+    while current_root is not None:
+        if key > current_root["key"]:
+            prev_root = current_root
+            current_root = current_root["right"]
+        elif key < current_root["key"]:
+            prev_root = current_root
+            current_root = current_root["left"]
+        else:
+            break
+
+    return prev_root, current_root
 
 
 def insert(key: int, value: Any) -> None:
@@ -15,8 +43,39 @@ def insert(key: int, value: Any) -> None:
     :param value: value associated with key
     :return: None
     """
-    print(key, value)
-    return None
+    global root
+
+    prev, current = _find(key)
+    if current is not None:
+        current["value"] = value
+    elif prev is None:
+        root = _create_node(key, value)
+    else:
+        if key > prev["key"]:
+            prev["right"] = _create_node(key, value)
+        else:
+            prev["left"] = _create_node(key, value)
+
+    # if root is None:
+    #     root = _create_node(key, value)
+    # return
+    # current_root = root
+    # while True:
+    #     if key > current_root["key"]:
+    #         if current_root["right"] is None:
+    #             current_root["right"] = _create_node(key, value)
+    #             return
+    #         else:
+    #             current_root = current_root["right"]
+    #     elif key < current_root["key"]:
+    #         if current_root["left"] is None:
+    #             current_root["left"] = _create_node(key, value)
+    #             return
+    #         else:
+    #             current_root = current_root["left"]
+    #     else:
+    #         current_root["value"] = value
+    #         return
 
 
 def remove(key: int) -> Optional[Tuple[int, Any]]:
@@ -26,8 +85,20 @@ def remove(key: int) -> Optional[Tuple[int, Any]]:
     :param key: key to be removed
     :return: deleted (key, value) pair or None
     """
-    print(key)
-    return None
+    global root
+
+    prev, current = _find(key)
+
+    if current is None:
+        return None
+    elif key > prev["key"] and prev["right"] is not None:
+        if current is not None:
+            current["right"] = None
+    elif key < prev["key"] and prev["left"] is not None:
+        if current is not None:
+            current["left"] = None
+    # else:
+    #     current["key"] = None
 
 
 def find(key: int) -> Optional[Any]:
@@ -37,8 +108,23 @@ def find(key: int) -> Optional[Any]:
     :param key: key for search in the BST
     :return: value associated with the corresponding key
     """
-    print(key)
-    return None
+    global root
+
+    if root is None:
+        raise KeyError("Нет такого значения")
+
+    current_node = root
+    while True:
+        if key == current_node["key"]:
+            return current_node["key"]
+        elif key > current_node["key"]:
+            if current_node["right"] is None:
+                raise KeyError("Нет такого значения")
+            current_node = current_node["right"]
+        elif key < current_node["key"]:
+            if current_node["left"] is None:
+                raise KeyError("Нет такого значения")
+            current_node = current_node["left"]
 
 
 def clear() -> None:
@@ -47,4 +133,24 @@ def clear() -> None:
 
     :return: None
     """
-    return None
+    global root
+    root = None
+
+
+def main():
+    clear()
+    insert(10, 10)
+    print(root)
+    insert(15, 15)
+    print(root)
+    insert(5, 5)
+    print(root)
+    insert(20, 20)
+    print(root)
+    print(find(10))
+    remove(5)
+    print(root)
+
+
+if __name__ == '__main__':
+    main()
